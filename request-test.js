ef3c68ea1229c22e63fc2a161237f9c660ac274b
Mike Parish <mtparishto.gmail.com>
2023-03-06 14:00:28 -0500

    more serve

---
+++ request-test.js
@@ -0,0 +1,27 @@
+"use-strict";
+
+const axios = require("axios");
+
+const book = {
+  'author':'Tracey Parish',
+    'title':'Good Stuff part 2',
+    'published':'1952-01-01'
+};
+
+axios.post('http://localhost:3000/books', book)
+.then((result) => {
+  console.log(result);
+  
+}).catch((err) => {
+  console.log(err);  
+});
+/*
+axios
+  .put("http://localhost:3000/books/3", book)
+  .then((response) => {
+    console.log("Response is: ", response);
+  })
+  .catch((error) => {
+    console.log("Error is: ", error);
+  });
+*/
